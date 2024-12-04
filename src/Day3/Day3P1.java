34bf96b1fa8a694f754f87e27b2f73bb5b1ed2cb
Isak Holmström <isak.holmstrom@rejlers.se>
2024-12-04 07:50:32 +0100

    Day 3 Puzzle 1

--- src/Day3/Day3P1.java
+++ src/Day3/Day3P1.java
@@ -6,40 +6,37 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Day3P1 {
     public static void main(String[] args) {
-        ArrayList<String> extracted = new ArrayList<>();
         int total = 0;
 
         try {
             Scanner scanner = new Scanner(new File("./src/Day3/PuzzleInput.txt"));
             while(scanner.hasNext()) {
                 String memory = scanner.nextLine();
-                for (int i = 0; i < memory.length(); i++) {
-                    int startIndex = memory.indexOf("mul(", i);
-                    if (startIndex > 0) {
-                        int endIndex = memory.indexOf(")", startIndex + 4) + 1;
-                        if (endIndex < startIndex + 13) {
-                            String mul = memory.substring(startIndex, endIndex);
-                            extracted.add(mul);
-                            i = startIndex + mul.length() - 1;
-                        }
-                    }
+                ArrayList<String> extracted = new ArrayList<>();
+
+                Matcher m = Pattern.compile("(mul\\(\\d{1,3},\\d{1,3}\\))").matcher(memory);
+                while (m.find()) {
+                    extracted.add(m.group());
                 }
 
-                for (String current : extracted) {
-                    current = current.substring(current.indexOf("(") + 1, current.indexOf(")"));
-                    System.out.println(current);
-                    if (current.indexOf(",") > 1 && current.indexOf(",") < 5) {
-                        List<Integer> ints = Arrays.stream(current.split(",")).map(Integer::parseInt).toList();
-                        total += ints.get(0) * ints.get(1);
+                for (int i = 0; i < extracted.size(); i++) {
+                    m = Pattern.compile("\\d+").matcher(extracted.get(i));
+                    ArrayList<Integer> ints = new ArrayList<>();
+                    while (m.find()) {
+                        ints.add(Integer.parseInt(m.group()));
                     }
-                }
+                    System.out.println(ints);
 
-                System.out.println(total);
+                    total += ints.get(0) * ints.get(1);
+                }
             }
+            System.out.println(total);
         } catch (FileNotFoundException e) {
             System.out.println(e);
         }
